94a89afc98d878e1233f5f09db9802e258fa5e1c
Christer Olsen <130916986+christerolsen@users.noreply.github.com>
2024-05-18 23:05:49 +0200

    Created useApi.jsx
    Created and tested useApi.jsx, and included useApi in HomePage.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,35 +1,20 @@
-import Button from "../../components/Button";
+import useApi from "../../hooks/useApi";
 
 const HomePage = () => {
   document.title = "eComify | Home";
-
-  return (
-    <>
-      <h1>Home H1</h1>
-      <p>Main is where the main content of the page will display</p>
-      <h2>And here's H2</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem quaerat,
-        temporibus doloribus voluptatum consectetur iste optio quam? Non ipsa
-        saepe quasi, temporibus eveniet ullam ipsum sint molestiae amet,
-        doloremque quae.
-      </p>
-      <h3>Where this is H3</h3>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem quaerat,
-        temporibus doloribus voluptatum consectetur iste optio quam? Non ipsa
-        saepe quasi, temporibus eveniet ullam ipsum sint molestiae amet,
-        doloremque quae.
-      </p>
-      <p>
-        <a href="#">This is a link</a>
-      </p>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="accent">Accent</Button>
-      <Button variant="disabled">Disabled</Button>
-    </>
+  const { data, isLoading, isError } = useApi(
+    "https://v2.api.noroff.dev/online-shop"
   );
+
+  if (isLoading) {
+    return <>Loading</>;
+  }
+
+  if (isError) {
+    return <>Error</>;
+  }
+
+  return <>Data Loaded Successfully!</>;
 };
 
 export default HomePage;
